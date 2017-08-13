e41e4c207971c643cc18f4e27a321241f64b4475
Nathan Smith <nathan@nathansmith.io>
2017-08-13 14:00:50 -0700

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jquery: true,
+    jest: true
+  },
+  extends: ["airbnb", "prettier", "prettier/react"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      error,
+      {
+        singleQuote: true,
+        trailingComma: "es5",
+        jsxBracketSameLine: true
+      }
+    ]
+  }
+};
